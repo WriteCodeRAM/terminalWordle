af2a507763de1f8906fe7c910b62063e6ca77e94
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2023-09-01 14:27:01 -0400

    Includes project from Capital One's TMM and other nodeJS exercises

--- project-code-along/game.js
+++ project-code-along/game.js
@@ -1,13 +1,126 @@
+//imports
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import chalkAnimation from 'chalk-animation';
+import figlet from 'figlet';
+import gradient from 'gradient-string';
+import { createSpinner } from 'nanospinner';
+
 // game logic
 
 //helper to resolve animations
 //ms = 2000 miliseconds, after 2 seconds, the promise will resolve
-const resolveAnimations = (ms = 2000) => new Promise((resolve) => setTimeout(resolve, ms));
+let playerName;
+const spinner = createSpinner('process loading. . .');
 
+const resolveAnimations = (ms = 2000) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
 
-
-async function main() {
-    //invoke our game functions here
+//start game function
+async function startGame() {
+  //welcome message utilizing the chalk animation package
+  const welcomeMsg = chalkAnimation.rainbow(
+    `welcome to the choose your adventure game \n`
+  );
+  //call helper
+  await resolveAnimations();
+  //stop the animation
+  welcomeMsg.stop();
+  //prompt for the game
+  console.log(`
+    ${chalk.bgGreenBright('we shall begin')}
+    this adventure lives in your terminal
+    if you choose any of the wrong choices, I will ${chalk.bgRed('terminate')}
+    if you make it to the end, you will be rewarded
+    `);
 }
 
-main()
+async function pathQuestion() {
+  const answers = await inquirer.prompt({
+    name: 'question_1',
+    type: 'list',
+    message: `Welcome ${playerName}, what path will you choose? \n`,
+    choices: [
+      {
+        name: 'left - you hear a breeze echoing down the tunnel',
+        value: 'left',
+      },
+      {
+        name: 'right - you hear rocks crumbling in the distance',
+        value: 'right',
+      },
+      { name: 'straight - you hear an eerie silence', value: 'straight' },
+    ],
+  });
+
+  //left question logic
+  async function leftQuestion() {
+    const answers = await inquirer.prompt({
+      name: 'leftQuestion',
+      type: 'list',
+      message: `You have two choices: \n`,
+      choices: ['Press the button', `Don't press the button`],
+    });
+    return handleLeftAnswer(answers.leftQuestion == 'Press the button');
+  }
+  //handle user choice
+  async function handleLeftAnswer(choice) {
+    spinner.start();
+    await resolveAnimations();
+    if (choice) {
+      spinner.success({
+        text: `a secret passage opens, ${playerName}, you made the right choice`,
+      });
+      await finalQuestion();
+    } else {
+      spinner.warn({
+        text: `${playerName}, you should have pressed the button. ${chalk.bgRed(
+          `FATAL - terminating process due to inactivity`
+        )}`,
+      });
+      process.exit(1);
+    }
+  }
+
+  await pathChoice(answers.question_1);
+  //continue down the left path
+  if (answers.question_1 === 'left') {
+    leftQuestion();
+    //continue down the right path
+  } else if (answers.question_1 === 'right') {
+    rightQuestion();
+  }
+}
+
+async function pathChoice(choice) {
+  spinner.start();
+  await resolveAnimations();
+  //   spinner.stop();
+
+  if (choice === 'left') {
+    spinner.success({ text: `Interesting choice, ${playerName}, continue on` });
+  } else if (choice === 'right') {
+    spinner.warn({ text: `Interesting choice, ${playerName}, continue on` });
+  } else {
+    spinner.error({ text: chalk.bgRed(`FATAL - terminating process`) });
+    process.exit(1);
+  }
+}
+
+async function playerInfo() {
+  const answers = await inquirer.prompt({
+    name: 'player_name',
+    type: 'input',
+    message: 'Hello, please enter your name.',
+  });
+  playerName = answers.player_name;
+  await pathQuestion();
+}
+
+async function main() {
+  //invoke our game functions here
+  await startGame();
+  await playerInfo();
+}
+
+main();
--- project/app.js
+++ project/app.js
@@ -1,1 +1,258 @@
 //write your code here
+import chalk from 'chalk';
+import axios from 'axios';
+import figlet from 'figlet';
+import inquirer from 'inquirer';
+import readline from 'readline';
+
+//stats for summary page
+let gamesPlayed = 0;
+let gamesWon = 0;
+let streak = 0;
+
+//maybe add inquirer for wordlength options
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const fetchWord = async () => {
+  let wordLength;
+  const wordLengthPrompt = await inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'length',
+        message: 'Select a word length',
+        choices: [4, 5, 6],
+      },
+    ])
+    .then((answers) => {
+      wordLength = answers.length;
+    });
+
+  //rapid api endpoint
+  const options = {
+    method: 'GET',
+    url: 'https://random-words5.p.rapidapi.com/getRandom',
+    params: { wordLength: wordLength },
+    headers: {
+      'X-RapidAPI-Key': 'dc93ee594cmsh3d384c14d13a9edp19dd7fjsn4cb68636655f',
+      'X-RapidAPI-Host': 'random-words5.p.rapidapi.com',
+    },
+  };
+
+  try {
+    // console.log(`this is the length: ${wordLength} in try statement`);
+    const response = await axios.request(options);
+    return response.data.toUpperCase();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const gameRules = () => {
+  figlet('Welcome!', function (err, data) {
+    if (err) {
+      console.log('Error:', err);
+      return;
+    }
+
+    console.log(data);
+
+    console.log(chalk.green('\nEasy: 5 Chances'));
+    console.log(chalk.blue('Medium: 4 Chances'));
+    console.log(chalk.red('Hard: 3 Chances'));
+    console.log(chalk.blackBright('IMPOSSIBLE: 1 CHANCE\n'));
+
+    //get difficulty from user
+    const difficultyPrompt = inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'difficulty',
+          message: 'Select a difficulty',
+          choices: ['Easy', 'Medium', 'Hard', 'IMPOSSIBLE'],
+        },
+      ])
+      .then((answers) => {
+        //display selection
+        console.log(`You selected difficulty: ${answers.difficulty}`);
+        if (answers.difficulty == 'Easy') {
+          console.log(chalk.green('You have 5 chances to guess the word'));
+          gameLogic(5);
+        } else if (answers.difficulty == 'Medium') {
+          console.log(chalk.blue('You have 4 chances to guess the word'));
+          gameLogic(4);
+        } else if (answers.difficulty == 'Hard') {
+          console.log(chalk.yellow('You have 3 chances to guess the word'));
+          gameLogic(3);
+        } else {
+          console.log(chalk.red('You have 1 chance to guess the word'));
+          gameLogic(1);
+        }
+      });
+  });
+};
+
+//get user input
+const askForGuess = () => {
+  return new Promise((resolve) => {
+    rl.pause();
+    rl.question('Enter your guess: ', (input) => {
+      rl.resume();
+      resolve(input);
+    });
+  });
+};
+//===================================================//
+
+//main logic
+const gameLogic = async (lives) => {
+  let word = await fetchWord();
+  word = word.toUpperCase();
+  let tries = 0;
+  let wordArray = word.toUpperCase().split('');
+  let attempt = '';
+
+  // console.log(word);
+
+  while (lives > 0) {
+    const guess = await askForGuess(); // Wait for user input
+
+    for (let i = 0; i < word.length; i++) {
+      attempt = '';
+      //winning condition
+      if (word === guess.toUpperCase()) {
+        tries++;
+        gamesWon++;
+        console.log(
+          chalk.green(`congrats you guessed the word in ${tries} tries`)
+        );
+        streak++;
+        endGame(word, chalk.green);
+        return playAgain();
+      }
+
+      if (word[i] === guess[i].toUpperCase()) {
+        console.log(
+          chalk.green(
+            `Letter '${guess[i].toUpperCase()}' is in the correct spot (${
+              i + 1
+            })`
+          )
+        );
+
+        attempt += chalk.green(guess[i]);
+      } else if (wordArray.includes(guess[i].toUpperCase())) {
+        console.log(
+          chalk.yellow(
+            `Letter '${guess[
+              i
+            ].toUpperCase()}' is in the word but in the incorrect spot (${
+              i + 1
+            })`
+          )
+        );
+        attempt += chalk.yellow(guess[i]);
+      } else {
+        console.log(
+          chalk.red(`Letter '${guess[i].toUpperCase()}' is not in the word`)
+        );
+        attempt += chalk.red(guess[i]);
+      }
+      // console.log(attempt);
+    }
+    tries++;
+    lives--;
+    console.log(chalk.red(`${lives} chances remaining`));
+  }
+  streak = 0;
+  endGame(word, chalk.red);
+  playAgain();
+};
+//===================================================//
+
+//display the correct word
+const endGame = (word, color) => {
+  gamesPlayed++;
+  return figlet(word, function (err, data) {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+    console.log(color(data));
+  });
+};
+//===================================================//
+
+//play again prompt
+const playAgain = () => {
+  const playAgainPrompt = inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'play_again',
+        message: 'Would you like to play again?',
+        choices: ['yes', 'no'],
+      },
+    ])
+    .then((answers) => {
+      //display selection
+      console.log(`You selected: ${answers.play_again}`);
+      if (answers.play_again == 'yes') {
+        gameRules();
+      } else {
+        displaySummary();
+      }
+    });
+};
+//===================================================//
+
+//show summary of user stats!
+const displaySummary = () => {
+  figlet('G A M E     O V E R !  !  !', function (err, data) {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+    console.log(chalk.red(data));
+  });
+
+  figlet(`G A M E S     P L A Y E D : ${gamesPlayed}`, function (err, data) {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+
+    console.log(chalk.blue(data));
+  });
+
+  figlet(`G A M E S     W O N : ${gamesWon}`, function (err, data) {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+    console.log(chalk.cyan(data));
+  });
+
+  figlet(`C u r r e n t     S t r e a k : ${streak}`, function (err, data) {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+    streak > 0 ? console.log(chalk.green(data)) : console.log(chalk.red(data));
+  });
+};
+//===================================================//
+
+//start
+(() => {
+  gameRules();
+})();
